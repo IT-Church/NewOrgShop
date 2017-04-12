9d2559f5696c50c068eb0341b6aaf72a9b4b54f4
Paramonod <gamiysikdima@gmail.com>
2017-04-11 23:10:20 -0400

    Я тоже спать
    Написал DBHelper и понял что на что-то нормальное меня сегодня не
    хватит, утром буду мучать, у меня много времени

--- MasterProj/app/src/main/java/com/paramonod/kikos/pack/DatabaseHelper.java
+++ MasterProj/app/src/main/java/com/paramonod/kikos/pack/DatabaseHelper.java
@@ -1,107 +1,47 @@
 package com.paramonod.kikos.pack;
 
+import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    String DB_PATH = null;
-    private static String DB_NAME = "sqlTest.db";
-    private SQLiteDatabase myDataBase;
-    private final Context myContext;
+    private static final String LOG_TAG = "my_tag";
+
+    public static final String TABLE_NAME = "friends";
+
+    public static final String KEY_ID = "idShops";
+    public static final String KEY_NAME = "ShopName";
+    public static final String KEY_COORDX = "CoordX";
+    public static final String KEY_COORDY = "CoordY";
+    public static final String KEY_DESC = "Description";
+    public static final String KEY_UPDATE = "Update";
+
+    private static final String DATABASE_NAME = "ShopsDB";
+    private static final int DATABASE_VERSION = 1;
 
     public DatabaseHelper(Context context) {
-        super(context, DB_NAME, null, 10);
-        this.myContext = context;
-        this.DB_PATH = "/data/data/" + context.getPackageName() + "/" + "databases/";
-        Log.e("Path 1", DB_PATH);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
-
-    public void createDataBase() throws IOException {
-        boolean dbExist = checkDataBase();
-        if (dbExist) {
-        } else {
-            this.getReadableDatabase();
-            try {
-                copyDataBase();
-            } catch (IOException e) {
-                throw new Error("Error copying database");
-            }
-        }
-    }
-
-    private boolean checkDataBase() {
-        SQLiteDatabase checkDB = null;
-        try {
-            String myPath = DB_PATH + DB_NAME;
-            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
-        } catch (SQLiteException e) {
-        }
-        if (checkDB != null) {
-            checkDB.close();
-        }
-        return checkDB != null ? true : false;
-    }
-
-    private void copyDataBase() throws IOException {
-        InputStream myInput = myContext.getAssets().open(DB_NAME);
-        String outFileName = DB_PATH + DB_NAME;
-        OutputStream myOutput = new FileOutputStream(outFileName);
-        byte[] buffer = new byte[10];
-        int length;
-        while ((length = myInput.read(buffer)) > 0) {
-            myOutput.write(buffer, 0, length);
-        }
-        myOutput.flush();
-        myOutput.close();
-        myInput.close();
-
-    }
-
-    public void openDataBase() throws SQLException {
-        String myPath = DB_PATH + DB_NAME;
-        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
-
-    }
-
-    @Override
-    public synchronized void close() {
-        if (myDataBase != null)
-            myDataBase.close();
-        super.close();
-    }
-
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table " + TABLE_NAME + " ("
+                + KEY_ID + " integer primary key autoincrement,"
+                + KEY_NAME + " text,"
+                + KEY_COORDX + " double,"
+                + KEY_COORDY + " double,"
+                + KEY_DESC + "text,"
+                + KEY_UPDATE + "integer" + ");");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (newVersion > oldVersion)
-            try {
-                copyDataBase();
-            } catch (IOException e) {
-                e.printStackTrace();
-
-            }
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        this.onCreate(db);
     }
-
-    public Cursor query(String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
-        return myDataBase.query(table, columns, selection, selectionArgs, groupBy, having, orderBy);
-    }
-
-
 }
